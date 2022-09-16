a6159dfccefc5af98ad8c4a00d5150da3641a6a6
AndersonAngelo <anderson_angelo1@hotmail.com>
2022-09-15 21:43:27 -0300

    added initial page of app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import {
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black 
+} from '@expo-google-fonts/inter'
+import { Backgorund } from './src/components/Backgorund';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  })
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-    </View>
+    <Backgorund>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Backgorund>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
